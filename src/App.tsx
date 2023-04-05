8d698bf17490c1c2ccd63ebb3750de075c3c93e6
Elias Montauti <egsm147@gmail.com>
2023-04-05 10:31:28 +0100

    5%

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import { Header } from './components/Header/Header';
 import Footer from './components/Footer';
 import Iphone14 from './img/Iphone14Pro_Home-Sm.jpg'
+import { Carousel, Container, Row, Col } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
@@ -11,7 +12,25 @@ const App: React.FC = () => {
     <Header />
     <body className='main'>
      <h1 className='main_title'>Welcome to Nice Gadgets store!</h1>
-     <img src={Iphone14} alt="Iphone14ProBeyond" />
+      <Carousel>
+        <Carousel.Item>
+        <img
+          src={Iphone14}
+          alt="Iphone14ProBeyond"
+          className='d-block w-100'
+          />
+        </Carousel.Item>
+      </Carousel>
+      <Container>
+      <Row>
+        <Col xs={8}>
+          <h2 className='main_subtitle'>Brand new models</h2>
+        </Col>
+        <Col xs={4}>
+          <button>+</button>
+        </Col>
+      </Row>
+      </Container>
     </body>
     <Footer />
     </div>
