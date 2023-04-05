ddd82c4265a04fe22c62e54c49335204fbd87e1b
Elias Montauti <egsm147@gmail.com>
2023-04-05 12:01:29 +0100

    5%

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import { Header } from './components/Header/Header';
-import Footer from './components/Footer';
-import Iphone14 from './img/Iphone14Pro_Home-Sm.jpg'
-import { Carousel, Container, Row, Col } from 'react-bootstrap';
+
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-    <Header />
-    <body className='main'>
-     <h1 className='main_title'>Welcome to Nice Gadgets store!</h1>
-      <Carousel>
-        <Carousel.Item>
-        <img
-          src={Iphone14}
-          alt="Iphone14ProBeyond"
-          className='d-block w-100'
-          />
-        </Carousel.Item>
-      </Carousel>
-      <Container>
-      <Row>
-        <Col xs={8}>
-          <h2 className='main_subtitle'>Brand new models</h2>
-        </Col>
-        <Col xs={4}>
-          <button>+</button>
-        </Col>
-      </Row>
-      </Container>
-    </body>
-    <Footer />
-    </div>
+      <div className="App">
+        <main className='section'>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <HomePage />
+              }
+            />
+          </Routes>
+        </main>
+      </div>
   );
 }
 
