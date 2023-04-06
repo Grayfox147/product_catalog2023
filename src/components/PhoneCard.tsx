15ffe49c4d8a63d3768b532f873d11fb79024877
Elias Montauti <egsm147@gmail.com>
2023-04-06 13:46:49 +0100

    10%

--- src/components/PhoneCard.tsx
+++ src/components/PhoneCard.tsx
@@ -4,18 +4,20 @@ import {
   Button,
 } from 'react-bootstrap';
 import Iphone14 from '../img/Iphone 14 Silver 128Gb.png';
+import { FiHeart } from 'react-icons/fi';
 
 export const PhoneCard: React.FC = () => {
   return (
     <Card className='card'>
-      <Card.Img variant='top' src={Iphone14} />
+      <Card.Img variant='top' src={Iphone14} className='card_image'/>
       <Card.Body>
-        <Card.Title style={{ color: "#f1f2f9" }}>
+        <Card.Title style={{ color: "#f1f2f9" }} className='card_title'>
           Apple iPhone 14 Pro 128GB Silver (MQ023)
         </Card.Title>
-        <Card.Subtitle style={{ color: "#f1f2f9" }}>
+        <Card.Subtitle style={{ color: "#f1f2f9" }} className='card_subtitle'>
           $999
         </Card.Subtitle>
+        <div className='card_line'></div>
         <div className='card_text_container'>
           <div className='card_text_left'>
             Screen <br />
@@ -29,8 +31,8 @@ export const PhoneCard: React.FC = () => {
           </div>
         </div>
         <Button>Add to cart</Button>
-        <button>
-          <i className="fa fa-heart"/>
+        <button className='like_button'>
+          <FiHeart className='heart'/>
         </button>
       </Card.Body>
     </Card>
