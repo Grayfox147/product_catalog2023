a84fa2a74263b19a998b098c3b18b18977fc3042
Elias Montauti <egsm147@gmail.com>
2023-04-12 15:13:05 +0100

    footerImplementation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,33 @@
 import * as React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
+// import { Container, Row, Col} from 'react-bootstrap';
+import footerLogo from '../Icons/logo__header_phone.svg';
 
 const Footer: React.FC = () => {
   return (
     <footer>
-      <Container className='footer_container'>
-        <Row>
-          <Col className='text-center'>
-          Copyright &copy;
-          </Col>
-        </Row>
-      </Container>
+      <div className='footer_container'>
+          <div className='footer_logo_container'>
+            <img
+              src={footerLogo}
+              alt="logo"
+              className='footer_logo'
+            />
+          </div>
+          <div className='f'>
+            <div className='footer_subcontainer'>
+              <a href="#github" className='footer_link'>GITHUB</a>
+            </div>
+            <div className='footer_subcontainer'>
+                <a href="#contacts" className='footer_link'>CONTACTS</a>
+            </div>
+            <div className='footer_subcontainer'>
+              <a href="#rigths" className='footer_link'>RIGTHS</a>
+            </div>
+          </div>
+          <div>
+            <a href="#backToTop" className='footer_Backlink'>Back to top</a>
+          </div>
+      </div>
     </footer>
    );
 }
