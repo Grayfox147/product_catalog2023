755c0a263dd7be81ac4040045c70657bec20e488
Elias Montauti <egsm147@gmail.com>
2023-04-18 15:27:44 +0100

    tablet-screen-25%overal

--- src/components/PhoneList/PhoneList.tsx
+++ src/components/PhoneList/PhoneList.tsx
@@ -8,14 +8,14 @@ type PhonesListProps = {
 };
 
 export const PhoneList: React.FC<PhonesListProps> = ({ phones }) => {
-	return (
-		<Row lg={4} md={2} x-small={1} className="phone_list">
-			{phones.map((phone) => (
-				<PhoneCard
-					phone={phone}
-					key={phone.id}
-				/>
-			))}
-		</Row>
-	);
+    return (
+        <Row lg={4} md={2} x-small={1} className="phone_list">
+            {phones.map((phone) => (
+                <PhoneCard
+                    phone={phone}
+                    key={phone.id}
+                />
+            ))}
+        </Row>
+    );
 };
