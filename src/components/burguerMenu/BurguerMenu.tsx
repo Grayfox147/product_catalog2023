a1001c4c1ea337b2a7b23ac761b0262f41f88e04
Elias Montauti <egsm147@gmail.com>
2023-04-19 12:37:18 +0100

    navbar85%

---
+++ src/components/burguerMenu/BurguerMenu.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FiHeart } from 'react-icons/fi';
+import { AiOutlineInbox } from 'react-icons/ai';
+
+export const BurguerMenu: React.FC = () => {
+    return (
+        <div>
+            <div className='burguer_links'>
+                <ul>
+                    <li>
+                        <a href="#home">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#phones">PHONES</a>
+                    </li>
+                    <li>
+                        <a href="#tablets">TABLETS</a>
+                    </li>
+                    <li>
+                        <a href="#accesories">ACCESSORIES</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='burguer_btns'>
+                <button>
+                    <FiHeart color='#F1F2F9' size={10}/>
+                </button>
+                <button>
+                    <AiOutlineInbox color='#F1F2F9' size={10}/>
+                </button>
+            </div>
+        </div>
+    );
+};
