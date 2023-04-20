04b9eac05027f0ed72a00c02082b642fa19d1adb
Elias Montauti <egsm147@gmail.com>
2023-04-20 11:41:35 +0100

    TabletView95%

--- src/components/PhoneCard/PhoneCard.tsx
+++ src/components/PhoneCard/PhoneCard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
     Card,
     Button,
-    Col,
 } from 'react-bootstrap';
 import { FiHeart } from 'react-icons/fi';
 import { Phone } from '../../pages/HomePage';
@@ -22,37 +21,35 @@ export const PhoneCard: React.FC<PhoneCardProps> = ({ phone }) => {
     }=phone;
 
     return (
-        <Col className='d-flex'>
-            <Card className='card flex-fill'>
-                <Card.Img variant='top' src={`product_catalog2023/${image}`} className='card_image'/>
-                <Card.Body>
-                    <Card.Title style={{ color: '#f1f2f9' }} className='card_title'>
-                        {name}
-                    </Card.Title>
-                    <Card.Subtitle className='card_subtitle'>
+        <Card className='card'>
+            <Card.Img variant='top' src={`product_catalog2023/${image}`} className='card_image'/>
+            <Card.Body>
+                <Card.Title style={{ color: '#f1f2f9' }} className='card_title'>
+                    {name}
+                </Card.Title>
+                <Card.Subtitle className='card_subtitle'>
                       ${price}
-                    </Card.Subtitle>
-                    <div className='card_line'></div>
-                    <div className='card_text_container'>
-                        <div className='card_text_left'>
+                </Card.Subtitle>
+                <div className='card_line'></div>
+                <div className='card_text_container'>
+                    <div className='card_text_left'>
                           Screen <br />
                           Capacity <br />
                           RAM
-                        </div>
-                        <div className='card_text_rigth'>
-                            {screen}<br />
-                            {capacity} <br />
-                            {ram}
-                        </div>
                     </div>
-                    <div className='card_btn_container'>
-                        <Button>Add to cart</Button>
-                        <button className='like_button'>
-                            <FiHeart className='heart'/>
-                        </button>
+                    <div className='card_text_rigth'>
+                        {screen}<br />
+                        {capacity} <br />
+                        {ram}
                     </div>
-                </Card.Body>
-            </Card>
-        </Col>
+                </div>
+                <div className='card_btn_container'>
+                    <Button>Add to cart</Button>
+                    <button className='like_button'>
+                        <FiHeart className='heart'/>
+                    </button>
+                </div>
+            </Card.Body>
+        </Card>
     );
 };
